deeb6339000b9b63403e14a01253d522541e483b
Gisellvanegas <productofficerads@fundacioncapital.org>
2025-05-03 16:18:44 -0500

    CRUD completo con estilos aplicados

--- desayunos/urls.py
+++ desayunos/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('crear/', views.crear_desayuno, name='crear_desayuno'),
+    path('eliminar/<int:desayuno_id>/', views.eliminar_desayuno, name='eliminar_desayuno'),
     path('listar/', views.listar_desayunos, name='listar_desayunos'),
+    path('editar/<int:desayuno_id>/', views.editar_desayuno, name='editar_desayuno'),
 ]
--- desayunos/views.py
+++ desayunos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import DesayunoForm
 from .models import Desayuno
 
@@ -15,3 +15,21 @@ def crear_desayuno(request):
 def listar_desayunos(request):
     desayunos = Desayuno.objects.all()
     return render(request, 'desayunos/listar_desayunos.html', {'desayunos': desayunos})
+
+def eliminar_desayuno(request, desayuno_id):
+    desayuno = get_object_or_404(Desayuno, id=desayuno_id)
+    desayuno.delete()
+    return redirect('listar_desayunos')
+
+def editar_desayuno(request, desayuno_id):
+    desayuno = get_object_or_404(Desayuno, id=desayuno_id)
+
+    if request.method == 'POST':
+        form = DesayunoForm(request.POST, instance=desayuno)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_desayunos')
+    else:
+        form = DesayunoForm(instance=desayuno)
+
+    return render(request, 'desayunos/editar_desayuno.html', {'form': form, 'desayuno': desayuno})
